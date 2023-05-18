022b23f35b656e1e6431887a0eb5855b3b8ec829
Philippe CHARLES <philippe.charles@axa.fr>
2023-05-18 13:57:16 +0200

    Ajoutez des fichiers projet.

---
+++ p9/Models/Reparation.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace p9.Models
+{
+    public class Reparation
+    {
+
+
+
+        [Key]
+
+
+        public int Id { get; set; }
+
+        [Required]
+
+        [DataType(DataType.Date)]
+
+        [DisplayName("Date d'intervention")]
+
+        public DateTime DateReparation { get; set; }
+
+        [Required]
+
+        [DisplayName("Type d'intervention")]
+
+        public string TypeIntervention { get; set; }
+
+        [Required]
+
+        [DataType(DataType.Currency)]
+
+        [DisplayName("Coût des réparations")]
+
+        public float CoutReparation { get; set; }
+
+        public int VoitureId { get; set; }
+
+        public Voiture? Voiture { get; set; }
+
+
+
+
+
+
+    }
+}
---
+++ p9/Models/Voiture.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace p9.Models
+{
+    public class Voiture
+    {
+        
+        [Key]
+        [DisplayName("Code VIN")]
+        public int Id { get; set; }
+        [Required]
+        [Range(1990, int.MaxValue)]
+        [DisplayName("Année")]
+        public int Annee { get; set; }
+        [Required]
+        [DisplayName("Marque")]
+        public string Marque { get; set; }
+        [Required]
+        [DisplayName("Modèle")]
+        public string Modele { get; set; }
+        [Required]
+        [DisplayName("Finition")]
+        public string Finition { get; set; }
+        public string MarqueModeleFinition => $"{Marque} {Modele} {Finition}";
+        [DataType(DataType.Date)]
+        [DisplayName("Date d'achat")]
+        public DateTime DateAchat { get; set; }
+        [DataType(DataType.Currency)]
+        [DisplayName("Prix d'achat")]
+        public float? PrixAchat { get; set; }
+        [DataType(DataType.Currency)]
+        [DisplayName("Prix de vente")]
+        public float? PrixVente { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayName("Date de vente")]
+        public DateTime? DateVente { get; set; }
+        [DisplayName("Véhicule disponible")]
+        public bool EstDisponible { get; set; }
+        public string? Description { get; set; }
+        public string? NomPhoto { get; set; }
+        public string? CheminPhoto { get; set; }
+        [NotMapped]
+        public IFormFile? Photo { get; set; }
+        public ICollection<Reparation>? Reparations { get; set; }
+
+        
+    }
+}
