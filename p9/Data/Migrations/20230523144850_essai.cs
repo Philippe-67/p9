08670ca401cda45ed6297ef659608035f2e021c3
Philippe CHARLES <philippe.charles@axa.fr>
2023-05-24 10:38:26 +0200

    annulation de deux push

---
+++ p9/Data/Migrations/20230523144850_essai.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace p9.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class essai : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Voitures_Utilisateurs_UtilisateurId",
+                table: "Voitures");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UtilisateurId",
+                table: "Voitures",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Voitures_Utilisateurs_UtilisateurId",
+                table: "Voitures",
+                column: "UtilisateurId",
+                principalTable: "Utilisateurs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Voitures_Utilisateurs_UtilisateurId",
+                table: "Voitures");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UtilisateurId",
+                table: "Voitures",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Voitures_Utilisateurs_UtilisateurId",
+                table: "Voitures",
+                column: "UtilisateurId",
+                principalTable: "Utilisateurs",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ p9/Models/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace p9.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Veuillez saisir votre adresse e-mail.")]
+        [EmailAddress(ErrorMessage = "Veuillez saisir une adresse e-mail valide.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Veuillez saisir votre mot de passe.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool ShowInvalidCredentialsMessage { get; set; }
+    }
+}
